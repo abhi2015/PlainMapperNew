b36650bdcc9b8c144443e7009b7be41ad454a5ac
Abhimanyu <arya.abhimanyu.92@gmail.com>
2015-02-06 01:12:39 +0530

    added move command specs

---
+++ spec/survey/move_command_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe "north command" do
+
+  let(:robot) {Robot.new(1,1,"NORTH")}
+
+  it "returns new direction" do
+    robot.north
+    expect(MoveCommand.new.execute(robot)).to eq([1,2]])
+  end
+  
+end
